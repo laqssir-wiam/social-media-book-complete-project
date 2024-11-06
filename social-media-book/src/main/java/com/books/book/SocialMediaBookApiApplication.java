47239d02aa59438fab945421246339ea2b0f769a
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-06 20:32:19 +0100

    social-media-book-api-v0

---
+++ social-media-book/src/main/java/com/books/book/SocialMediaBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.books.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaBookApiApplication.class, args);
+	}
+
+}
