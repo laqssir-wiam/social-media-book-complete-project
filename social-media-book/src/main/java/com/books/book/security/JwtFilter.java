35d41bd148ac7ca5a26317d2d547953272e277e1
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-11 16:15:10 +0100

    finalize the JwtFilter

--- social-media-book/src/main/java/com/books/book/security/JwtFilter.java
+++ social-media-book/src/main/java/com/books/book/security/JwtFilter.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -20,7 +19,7 @@ import java.io.IOException;
 /**
  * @author WIAM
  **/
- @Component
+@Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
