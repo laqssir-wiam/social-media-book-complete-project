cfb2c8ca0674051adcab798d857a8ac6289f852e
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-16 22:17:07 +0100

    book entity

---
+++ social-media-book/src/main/java/com/books/book/common/PageResponse.java
@@ -0,0 +1,29 @@
+package com.books.book.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author WIAM
+ **/
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+    private int number;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
+}
