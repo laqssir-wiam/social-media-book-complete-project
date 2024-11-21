cdbd122f69a4be8763cffd946143f99db954160b
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-21 20:50:03 +0100

    adding OperationNotPermittedException and updateArchivedStatus methode

--- social-media-book/src/main/java/com/books/book/book/BookController.java
+++ social-media-book/src/main/java/com/books/book/book/BookController.java
@@ -70,4 +70,11 @@ public class BookController {
     ) {
         return ResponseEntity.ok(service.updateShareableStatus(bookId, connectedUser));
     }
+    @PatchMapping("/archived/{book-id}")
+    public ResponseEntity<Integer> updateArchivedStatus(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.updateArchivedStatus(bookId, connectedUser));
+    }
 }
--- social-media-book/src/main/java/com/books/book/book/BookService.java
+++ social-media-book/src/main/java/com/books/book/book/BookService.java
@@ -132,4 +132,18 @@ public class BookService {
         bookRepository.save(book);
         return bookId;
     }
+    public Integer updateArchivedStatus(Integer bookId, Authentication connectedUser) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+        User user = ((User) connectedUser.getPrincipal());
+        if (!Objects.equals(book.getOwner().getId(), user.getId())) {
+            throw new OperationNotPermittedException("You cannot update others books archived status");
+        }
+//        if (!Objects.equals(book.getCreatedBy(), connectedUser.getName())) {
+//            throw new OperationNotPermittedException("You cannot update others books archived status");
+//        }
+        book.setArchived(!book.isArchived());
+        bookRepository.save(book);
+        return bookId;
+    }
 }
