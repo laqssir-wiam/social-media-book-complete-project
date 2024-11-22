f93c4b8c5b25dc96e562dc76da450bf8eae174e4
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-22 21:31:41 +0100

    adding approveReturnBorrowedBook methode

--- social-media-book/src/main/java/com/books/book/book/BookController.java
+++ social-media-book/src/main/java/com/books/book/book/BookController.java
@@ -91,5 +91,11 @@ public class BookController {
     ) {
         return ResponseEntity.ok(service.returnBorrowedBook(bookId, connectedUser));
     }
-
+    @PatchMapping("borrow/return/approve/{book-id}")
+    public ResponseEntity<Integer> approveReturnBorrowBook(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.approveReturnBorrowedBook(bookId, connectedUser));
+    }
 }
