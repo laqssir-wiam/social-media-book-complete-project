f19a92167b18e90fc14389ed91b5eb0dcb274f38
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-29 16:04:29 +0100

    feedback interface and save feedback method implimentation

---
+++ social-media-book/src/main/java/com/books/book/feedback/BookRepository.java
@@ -0,0 +1,7 @@
+package com.books.book.feedback;
+
+/**
+ * @author WIAM
+ **/
+public interface BookRepository {
+}
---
+++ social-media-book/src/main/java/com/books/book/feedback/FeedbackService.java
@@ -0,0 +1,30 @@
+package com.books.book.feedback;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author WIAM
+ **/
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+
+//    private final FeedBackRepository feedBackRepository;
+    private final BookRepository bookRepository;
+//    private final FeedbackMapper feedbackMapper;
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + request.bookId()));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("You cannot give a feedback for and archived or not shareable book");
+        }
+        // User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getCreatedBy(), connectedUser.getName())) {
+            throw new OperationNotPermittedException("You cannot give feedback to your own book");
+        }
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedBackRepository.save(feedback).getId();
+    }
+}
