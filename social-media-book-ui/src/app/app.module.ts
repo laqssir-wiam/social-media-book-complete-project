3dad5abea568cb0a45e25a3f58aed7a84550c88a
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-12-30 21:10:37 +0100

    add some changes in login configurstion

--- social-media-book-ui/src/app/app.module.ts
+++ social-media-book-ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -14,7 +14,8 @@ import { LoginComponent } from './pages/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
