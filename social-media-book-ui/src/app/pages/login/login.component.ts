67d2c61f6b885fa3ad52663507c8b5e21fe13ca6
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-12-31 19:07:05 +0100

    implementing login method

--- social-media-book-ui/src/app/pages/login/login.component.ts
+++ social-media-book-ui/src/app/pages/login/login.component.ts
@@ -21,23 +21,23 @@ export class LoginComponent {
 //     await this.ss.login();
   }
   login() {
-//     this.errorMsg = [];
-//     this.authService.authenticate({
-//       body: this.authRequest
-//     }).subscribe({
-//       next: (res) => {
-//         this.tokenService.token = res.token as string;
-//         this.router.navigate(['books']);
-//       },
-//       error: (err) => {
-//         console.log(err);
-//         if (err.error.validationErrors) {
-//           this.errorMsg = err.error.validationErrors;
-//         } else {
-//           this.errorMsg.push(err.error.errorMsg);
-//         }
-//       }
-//     });
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res) => {
+        // this.tokenService.token = res.token as string;
+        this.router.navigate(['books']);
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.errorMsg);
+        }
+      }
+    });
   }
 
   register() {
