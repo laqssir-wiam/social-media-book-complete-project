3a70aeabac1175fddb113d9780c3ee76bfa066cf
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-01-11 22:02:51 +0100

    activate-account correction

--- social-media-book-ui/src/app/pages/activate-account/activate-account.component.ts
+++ social-media-book-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -16,4 +16,29 @@ export class ActivateAccountComponent {
     private router: Router,
     private authService: AuthenticationService
   ){}
+  private confirmAccount(token: string) {
+    this.authService.confirm({
+      token
+    }).subscribe({
+      next: () => {
+        this.message = 'Your account has been successfully activated.\nNow you can proceed to login';
+        this.submitted = true;
+      },
+      error: () => {
+        this.message = 'Token has been expired or invalid';
+        this.submitted = true;
+        this.isOkay = false;
+      }
+    });
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+
+  protected readonly skipUntil = skipUntil;
 }
