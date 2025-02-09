6f84212167431a3996eead39f9cebeb44822c8f1
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-02-09 21:15:45 +0100

    devlopping methods in rating component

--- social-media-book-ui/src/app/modules/book/componenets/rating/rating.component.ts
+++ social-media-book-ui/src/app/modules/book/componenets/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './rating.component.scss'
 })
 export class RatingComponent {
+
+  @Input() rating : number =0;
+  maxRating: number = 5;
+  get fullStars():number{
+    return Math.floor(this.rating);
+  }
+  get hasHalfStar(): boolean{
+    return this.rating % 1 !== 0;
+  }
+  get emptyStars(): number{
+    return this.maxRating-Math.ceil(this.rating);
+  }
 
 }
