bba665675a1132eb87f27e3fd9d59c56ba8c455d
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-02-12 18:01:26 +0100

    updating openapi

--- social-media-book/src/main/java/com/books/book/book/BookController.java
+++ social-media-book/src/main/java/com/books/book/book/BookController.java
@@ -87,6 +87,7 @@ public class BookController {
         return ResponseEntity.ok(service.borrowBook(bookId, connectedUser));
     }
 
+
     @PatchMapping("borrow/return/{book-id}")
     public ResponseEntity<Integer> returnBorrowBook(
             @PathVariable("book-id") Integer bookId,
